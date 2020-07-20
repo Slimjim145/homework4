fdf37688727f844517c96c891dfdd0a746731301
Slimjim <fang241999@gmail.com>
2020-07-20 16:07:57 -0400

    tbc

---
+++ js/death.js
@@ -0,0 +1,58 @@
+
+var questions = [
+
+    {
+      title: "What is computer coding?",
+      choices: ["Is an output of functions", "A radio channel", "A tv show", "The instructions of a computer"],
+      answer: "The instructions of a computer"
+    },
+  
+    {
+      title: "?",
+      choices: ["Austria", "Switzerland", "Germany", "Russia"],
+      answer: "Germany"
+    },
+  
+    {
+      title: "Which Desert dominates a large area of Northern Africa?",
+      choices: ["Kalahari", "Sahara", "Gobi", "Atacama"],
+      answer: "Sahara"
+    },
+  
+    {
+      title: "What is the formal language of Libya?",
+      choices: ["English", "Spanish", "Arabic", "Dutch"],
+      answer: "Arabic"
+    },
+  
+    {
+      title: "The currents of which Ocean create the El Nino effect?",
+      choices: ["Pacific", "Indian", "Atlantic", "Arctic"],
+      answer: "Pacific"
+    },
+  
+    {
+      title: "In which Canadian province is Montreal located?",
+      choices: ["Vancouver", "Ontario", "Quebec", "Nova Scotia"],
+      answer: "Quebec"
+    },
+  
+    {
+      title: "What is the capital of South Korea?",
+      choices: ["Tokyo", "Beijing", "Seoul", "Phnom Penh"],
+      answer: "Seoul"
+    },
+  
+    {
+      title: "How many states starting with the letter A in the USA?",
+      choices: ["3", "4", "5", "7"],
+      answer: "4"
+    },
+
+];
+var startButton = document.getElementById("startBtn");
+startButton.addEventListener("click", function(){
+  if (click === true){
+    document.questions
+  }
+}
